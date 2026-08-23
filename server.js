b97cfbbcad4c245e5e39a5b34bbb76c06cbf50eb
vidhirpatel07-arch <vidhirpatel.07@gmail.com>
2026-08-23 13:22:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -459,7 +459,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Fallback for SPA routing
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
